7a89668c09562e81b1ba7b8c233c856abf6dc167
bfakhriyeh <betagenius@hotmail.com>
2025-06-10 00:09:00 +0330

    Updated - Updated feign config for jenkins.

--- thing/src/main/java/com/fkhr/thingsorganizer/thing/ThingApplication.java
+++ thing/src/main/java/com/fkhr/thingsorganizer/thing/ThingApplication.java
@@ -1,10 +1,12 @@
 package com.fkhr.thingsorganizer.thing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -14,12 +16,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.fkhr.thingsorganizer")
 @EnableCaching
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {
         "com.fkhr.thingsorganizer.commonsecurity.service",
         "com.fkhr.thingsorganizer.thing"})
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class ThingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThingApplication.class, args);
