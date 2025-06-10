bd13f61355add9b7da63feb77532e1720ec285d4
bfakhriyeh <betagenius@hotmail.com>
2025-06-11 00:02:49 +0330

    Added - Added db config to test for error in jenkins.

--- thing/src/test/java/com/fkhr/thingsorganizer/thing/repository/ThingRepositoryIT.java
+++ thing/src/test/java/com/fkhr/thingsorganizer/thing/repository/ThingRepositoryIT.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:sqlserver://host.docker.internal:1433;databaseName=things-organizer;encrypt=true;trustServerCertificate=true",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=fakhr",
+        "spring.datasource.driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver",
+        "spring.jpa.database-platform=org.hibernate.dialect.SQLServerDialect"
+})
 class ThingRepositoryIT {
     @Autowired
     ThingRepository thingRepository;
